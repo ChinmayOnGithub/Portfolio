0588d15926da73d057e942215809973e01db33ed
Chinmay <chinmay.patil3@walchandsangli.ac.in>
2024-09-30 00:04:52 +0530

    Under Construction

---
+++ react.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// A simple functional component
+function App() {
+    return (
+        <div>
+            <h1>Welcome to My Portfolio</h1>
+            <p>This is a simple React application.</p>
+        </div>
+    );
+}
+
