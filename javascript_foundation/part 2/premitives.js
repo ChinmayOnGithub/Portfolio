fb3f5fa002daf62592320cd5c6134f2713ed9902
Chinmay <chinmay.patil3@walchandsangli.ac.in>
2024-10-11 10:21:18 +0530

    Hitesh Chaudhary - Javascript♥️

---
+++ javascript_foundation/part 2/premitives.js
@@ -0,0 +1,39 @@
+// Numbers
+let balance = 120
+let anotherNumber = new Number(120)
+
+// console.log(balance);
+// console.log(anotherNumber);
+// console.log(anotherNumber.valueOf());
+
+// in js everything is Object
+
+
+// console.log(typeof (anotherNumber));
+// console.log(typeof (balance));
+
+// Boolean
+let isActive = false;
+// let isActive2 = new Boolean(false); // not recommended
+
+// null & undefined
+let firstname;
+// console.log(firstname);
+
+// strings
+
+let username = "chinmay"
+let lastname = 'patil'
+let para = username + " " + lastname
+console.log(username);
+console.log(lastname);
+console.log(para);
+
+let paragraph = `my name is ${username} ${lastname}.` // using those variables in there is called {String Interpolation}.
+console.log(paragraph);
+
+
+
+
+
+
