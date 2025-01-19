e76b43127ab6a3807b2ec683b45a29cd21c34d65
Chinmay <chinmay.patil3@walchandsangli.ac.in>
2025-01-19 11:08:24 +0530

    complete comment.controller.js

--- BACKEND/VIDTUBE/src/controllers/comment.controller.js
+++ BACKEND/VIDTUBE/src/controllers/comment.controller.js
@@ -22,9 +22,10 @@ const getVideoComments = asyncHandler(async (req, res) => {
     const totalComments = await Comment.countDocuments({ video: videoId });
 
     return res.status(200).json(new ApiResponse(200, "Fetched comments successfully", {
-      comments,
+      totalComments,
       totalPages: Math.ceil(totalComments / limit),
-      currentPage: page
+      currentPage: page,
+      comments
     }));
   } catch (error) {
     return res.status(500).json(new ApiError(500, "Something went wrong while fetching comments", error));
@@ -35,8 +36,13 @@ const addComment = asyncHandler(async (req, res) => {
   // TODO: add a comment to a video
   // user is already verified using auth function
 
-  const { videoId } = req.params
-  const { content } = req.body
+  const { videoId } = req.params;
+  const { content } = req.body;
+  const userId = req.user?._id;
+
+  if (!userId) {
+    return res.status(401).json(new ApiError(401, "User not authenticated"));
+  }
 
   if (!content) {
     return res
@@ -54,7 +60,7 @@ const addComment = asyncHandler(async (req, res) => {
     const comment = await Comment.create({
       content,
       video: videoId,
-      ower: req.user?._id
+      owner: userId
     });
 
     return res
@@ -66,18 +72,95 @@ const addComment = asyncHandler(async (req, res) => {
 
     return res
       .status(500)
-      .json(new ApiError(500, "Something went wrong", error));
+      .json(new ApiError(500, "Something went wrong while adding comment", error));
   }
 
 });
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
-})
+  const { videoId, commentId } = req.params
+  const { content } = req.body
+
+  if (!content) {
+    return res
+      .status(400)
+      .json(new ApiError(400, "Content is required"))
+  }
+  if (!videoId || !isValidObjectId(videoId)) {
+    return res
+      .status(400)
+      .json(new ApiError(400, "Video id is not valid"))
+  }
+
+  try {
+
+    if (!commentId || !isValidObjectId(commentId)) {
+      return res.status(400).json(new ApiError(400, "Comment id is not valid"));
+    }
+
+    const comment = await Comment.findOneAndUpdate(
+      {
+        _id: commentId,
+        video: videoId,
+        owner: req.user?._id
+      },
+      { content },
+      { new: true }
+    );
+
+    if (!comment) {
+      return res.status(404).json(new ApiError(404, "Comment not found"));
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Updated comment successfully", comment))
+
+  } catch (error) {
+    console.log("Cant update a comment");
+
+    return res
+      .status(500)
+      .json(new ApiError(500, "Something went wrong while updating the comment", error));
+  }
+
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
-})
+  const { videoId, commentId } = req.params
+
+  if (!videoId || !isValidObjectId(videoId)) {
+    return res
+      .status(400)
+      .json(new ApiError(400, "video id not valid"));
+  }
+  if (!commentId || !isValidObjectId(commentId)) {
+    return res
+      .status(400)
+      .json(new ApiError(400, "comment id not valid"));
+  }
+
+  try {
+    const comment = await Comment.findOneAndDelete(
+      {
+        video: videoId,
+        owner: req.user?._id,
+        _id: commentId
+      }
+    )
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Successfully deleted the comment", comment))
+
+  } catch (error) {
+    return res
+      .status(400)
+      .json(new ApiError(400, "Something went wrong while deleting the comment", error));
+  }
+});
 
 export {
   getVideoComments,
--- BACKEND/VIDTUBE/src/routes/comment.routes.js
+++ BACKEND/VIDTUBE/src/routes/comment.routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import {
   addComment,
-  getVideoComments
+  deleteComment,
+  getVideoComments,
+  updateComment
 } from "../controllers/comment.controller.js";
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -11,6 +13,8 @@ const router = Router()
 
 // secure routes
 router.route("/add-comment/:videoId").post(verifyJWT, addComment)
+router.route("/update-comment/:videoId/:commentId").patch(verifyJWT, updateComment)
+router.route("/delete-comment/:videoId/:commentId").delete(verifyJWT, deleteComment)
 
 
 
