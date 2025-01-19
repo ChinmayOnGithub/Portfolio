c647dc654d14c24540d8443a3ed69734d361c1a8
Chinmay <chinmay.patil3@walchandsangli.ac.in>
2025-01-19 14:50:14 +0530

    completed playlist.controller.js

--- BACKEND/VIDTUBE/src/routes/playlist.routes.js
+++ BACKEND/VIDTUBE/src/routes/playlist.routes.js
@@ -1,5 +1,13 @@
 import { Router } from "express"
-import { createPlaylist } from "../controllers/playlist.controller.js"
+import {
+  createPlaylist,
+  getPlaylistById,
+  getUserPlaylists,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  deletePlaylist,
+  updatePlaylist
+} from "../controllers/playlist.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 
@@ -7,6 +15,15 @@ const router = Router()
 
 // secure routes
 router.route("/create-playlist").post(verifyJWT, createPlaylist);
+
+
+// non secure routes
+router.route("/get-user-playlist/:userId").get(getUserPlaylists);
+router.route("/get-playlist-by-id/:playlistId").get(getPlaylistById);
+router.route("/add-video-to-playlist/:playlistId/:videoId").post(addVideoToPlaylist)
+router.route("/remove-video-from-playlist/:playlistId/:videoId").post(removeVideoFromPlaylist)
+router.route("/delete-playlist/:playlistId").delete(deletePlaylist)
+router.route("/update-playlist/:playlistId").patch(updatePlaylist)
 
 
 export default router;
